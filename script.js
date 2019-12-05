e47efd446c0baccfcd25039a55df4d0277ad6f55
cjconnor18 <cjallen1988@gmail.com>
2019-12-04 20:20:31 -0600

    validating inputs

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// Write your JavaScript code here!
+window.addEventListener("load",function(){// Write your JavaScript code here!
+  let form=document.querySelector('form');
+  form.addEventListener('submit',function(){
+    let pilotName=document.querySelector("input[name=pilotName]");
+    let copilotName=document.querySelector("input[name=copilotName]");
+    let fuelLevel=document.querySelector("input[name=fuelLevel]");
+    let cargoMass=document.querySelector("input[name=cargoMass]");
+    if(pilotName.value==="" || copilotName.value==="" || fuelLevel.value==="" || cargoMass.value===""){
+      alert("All fields are Required!");
+      event.preventDefault();
+    } else if(typeof(pilotName.value)!=='string' || typeof(copilotName.value)!=='string'){
+      alert("must be letters");
+      event.preventDefault();
+    } else if(isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoMass.value))){
+      alert("Fuel Level and Cargo Mass must only be numbers");
+      event.preventDefault();
+    }
 
+
+//look up if statements and template literals
+//add if and then add class if it is active to make it green...setAttribute???
+
+  })
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -11,3 +32,4 @@
 </ol>
 <img src="${}">
 */
+});
