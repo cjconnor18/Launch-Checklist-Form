38d2ff59c6e3221d077e02d274a0da14b641b47c
cjconnor18 <cjallen1988@gmail.com>
2019-12-08 13:48:36 -0600

    added js for entire project

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
 window.addEventListener("load",function(){// Write your JavaScript code here!
   let form=document.querySelector('form');
+  const fetchPromise = fetch("https://handlers.education.launchcode.org/static/planets.json");
+  fetchPromise.then( function(response) {
+    const jsonPromise = response.json();
+    jsonPromise.then( function(json) {
+      let selectedLocation=json[4];
+      let missionTarget=document.getElementById("missionTarget");
+      missionTarget.innerHTML=`
+        <h2>Mission Destination</h2>
+        <ol>
+          <li>Name: ${selectedLocation.name}</li>
+          <li>Diameter: ${selectedLocation.diameter}</li>
+          <li>Star: ${selectedLocation.star}</li>
+          <li>Distance from Earth: ${selectedLocation.distance}</li>
+          <li>Number of Moons: ${selectedLocation.moons}</li>
+        </ol>
+        <img src="${selectedLocation.image}">
+      `;
+    });
+  });
   form.addEventListener('submit',function(){
     let pilotName=document.querySelector("input[name=pilotName]");
     let copilotName=document.querySelector("input[name=copilotName]");
     let fuelLevel=document.querySelector("input[name=fuelLevel]");
     let cargoMass=document.querySelector("input[name=cargoMass]");
+
+    
+
+    let pilotStatus=document.getElementById("pilotStatus");
+    let copilotStatus=document.getElementById("copilotStatus");
+    let fuelStatus=document.getElementById("fuelStatus");
+    let cargoStatus=document.getElementById("cargoStatus");
+
+    let launchStatus=document.getElementById("launchStatus");
+    
+    function changeVisibility(){
+      let faultyItems=document.getElementById("faultyItems");
+      faultyItems.style.visibility="visible";
+    }
+    function launchNotReady(){
+      launchStatus.innerHTML=`Shuttle not ready for launch!`;
+      launchStatus.style.color='red';
+      event.preventDefault();
+    }
+    
+
     if(pilotName.value==="" || copilotName.value==="" || fuelLevel.value==="" || cargoMass.value===""){
       alert("All fields are Required!");
       event.preventDefault();
-    } else if(typeof(pilotName.value)!=='string' || typeof(copilotName.value)!=='string'){
-      alert("must be letters");
+    } else if(!isNaN(Number(pilotName.value))|| !isNaN(Number(copilotName.value))||isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoMass.value))){
+      alert("Make sure to enter valid information for each field!");
       event.preventDefault();
-    } else if(isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoMass.value))){
-      alert("Fuel Level and Cargo Mass must only be numbers");
-      event.preventDefault();
-    } else{
+    } 
     
-      let pilotStatus=document.getElementById("pilotStatus");
-      let copilotStatus=document.getElementById("copilotStatus");
-      pilotStatus.innerHTML=`Pilot ${pilotName} ready.`;
-      copilotStatus.innerHTML=`Copilot ${copilotName} ready.`;
-      let launchStatus=document.getElementById("launchStatus");
-
-      if(fuelLevel.value<"10000"){
-        let faultyItems=document.getElementById("faultyItems");
-        faultyItems.style.visibility="visible";
-        let fuelStatus=document.getElementById("fuelStatus");
-        fuelStatus.innerHTML=`Fuel too low for journey.`;
-        launchStatus.innerHTML=`Shuttle not ready for launch!`;
-        launchStatus.style.color='red';
-      }
+    pilotStatus.innerHTML=`Pilot ${pilotName.value} ready.`;
+    copilotStatus.innerHTML=`Copilot ${copilotName.value} ready.`;
+    function fuelTooLow(){
+      fuelStatus.innerHTML=`Fuel too low for journey!`;
+    }
+    function massTooHigh(){
+      cargoStatus.innerHTML=`Too much mass for the shuttle to take off!`
     }
 
-//look up if statements and template literals
-//add if and then add class if it is active to make it green...setAttribute???
-
-  })
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+    if(fuelLevel.value<"10000"||cargoMass.value>10000){
+      if(fuelLevel.value<"10000"&&cargoMass.value>10000){
+        fuelTooLow();
+        massTooHigh();
+      } else if(fuelLevel.value<"10000"){
+        fuelTooLow();
+      } else {
+        massTooHigh();
+      }
+      changeVisibility();
+      launchNotReady();
+    } else {
+      launchStatus.innerHTML=`Shuttle is ready for launch!`;
+      launchStatus.style.color='green';
+      event.preventDefault();
+    }
+  });
 });
