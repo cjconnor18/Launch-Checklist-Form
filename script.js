571332858646b5e2625c1fca49e5d93ad187b151
cjconnor18 <cjallen1988@gmail.com>
2019-12-08 11:15:28 -0600

    javascript check

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ window.addEventListener("load",function(){// Write your JavaScript code here!
     } else if(isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoMass.value))){
       alert("Fuel Level and Cargo Mass must only be numbers");
       event.preventDefault();
-    }
+    } else{
+    
+      let pilotStatus=document.getElementById("pilotStatus");
+      let copilotStatus=document.getElementById("copilotStatus");
+      pilotStatus.innerHTML=`Pilot ${pilotName} ready.`;
+      copilotStatus.innerHTML=`Copilot ${copilotName} ready.`;
+      let launchStatus=document.getElementById("launchStatus");
 
+      if(fuelLevel.value<"10000"){
+        let faultyItems=document.getElementById("faultyItems");
+        faultyItems.style.visibility="visible";
+        let fuelStatus=document.getElementById("fuelStatus");
+        fuelStatus.innerHTML=`Fuel too low for journey.`;
+        launchStatus.innerHTML=`Shuttle not ready for launch!`;
+        launchStatus.style.color='red';
+      }
+    }
 
 //look up if statements and template literals
 //add if and then add class if it is active to make it green...setAttribute???
